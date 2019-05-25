103dcabc227c384389d8c3b99416b7ae4fea4c4a
veronimineral <kabaczekey@gmail.com>
2019-05-25 15:11:30 +0200

    Add smooth scroll

--- js/script.js
+++ js/script.js
@@ -16,3 +16,12 @@ $(window).mousewheel(function (e) {
         }
     }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
